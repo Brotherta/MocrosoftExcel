b37e0dc6b53cc541e887e4ac2f40e6637b8eca39
Juliette S <juliette.sabatier07@gmail.com>
2020-12-19 17:21:06 +0100

    modifs course

--- src/backend/course/OptionCourse.java
+++ src/backend/course/OptionCourse.java
@@ -3,14 +3,14 @@ package backend.course;
 import java.util.List;
 
 public class OptionCourse extends AbstractCourse {
-    private List<SimpleCourse> optionList;
+    private List<Course> optionList;
 
-    OptionCourse(String id, String name, List<SimpleCourse> optionList){
+    public OptionCourse(String id, String name, List<Course> optionList){
         this.optionList = optionList;
         this.id = id;
         this.name = name;
         this.credits = optionList.get(0).getCredits();
     }
 
-    public List<SimpleCourse> getOptionList() { return this.optionList; }
+    public List<Course> getOptionList() { return this.optionList; }
 }
