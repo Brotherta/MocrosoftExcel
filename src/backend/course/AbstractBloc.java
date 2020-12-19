e0e8b98b2fedf0474e0c7e8e61727a53e56c4a19
Juliette S <juliette.sabatier07@gmail.com>
2020-12-19 12:55:31 +0100

    backend.course done

--- src/backend/course/AbstractBloc.java
+++ src/backend/course/AbstractBloc.java
@@ -1,4 +1,16 @@
 package backend.course;
 
 public abstract class AbstractBloc implements Bloc {
+
+    protected String id;
+    protected String name;
+    protected int credits;
+
+    @Override
+    public String getId() { return this.id; }
+    @Override
+    public String getName() { return this.name; }
+    @Override
+    public int getCredits() { return this.credits; }
+
 }
--- src/backend/course/Course.java
+++ src/backend/course/Course.java
@@ -1,4 +1,18 @@
 package backend.course;
 
 public class Course {
+
+    private String id;
+    private String name;
+    private int credits;
+
+    Course(String id, String name, int credits) {
+        this.id = id;
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public String getId() { return this.id; }
+    public String getName() { return this.name; }
+    public int getCredits() { return this.credits; }
 }
