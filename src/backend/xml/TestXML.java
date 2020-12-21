664f409abca7320138562dd964de9fa8947a2a53
Brotherta <57185748+Brotherta@users.noreply.github.com>
2020-12-21 19:47:06 +0100

    On peut écrire mais pas modifier pour le moment

--- src/backend/xml/TestXML.java
+++ src/backend/xml/TestXML.java
@@ -1,11 +1,19 @@
 package backend.xml;
 
+import backend.course.CompositeCourse;
+import backend.course.Course;
+import backend.course.OptionCourse;
+import backend.course.SimpleCourse;
+import backend.program.Program;
+import backend.student.Grade;
+import backend.student.Student;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.swing.text.html.Option;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,5 +23,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestXML {
+    public static void main(String[] args) {
+        SimpleCourse c1 = new SimpleCourse("66666", "salut", 9);
+        SimpleCourse c2 = new SimpleCourse("99999", "ça va ?", 10);
+        List<SimpleCourse> l = new ArrayList<>();
+        l.add(c1);
+        l.add(c2);
 
+        Save<Object> save = new Save<>();
+
+        save.addBuffer(c1);
+        save.addBuffer(c2);
+
+        List<Grade> grades = new ArrayList<>();
+        Grade g1 = new Grade(0.0, c1);
+        Grade g2 = new Grade(19.15, c2);
+        grades.add(g1);
+        grades.add(g2);
+
+        Student student = new Student("antoine", "Vidal", "21702760", "pas de prog", grades);
+        save.addBuffer(student);
+
+        List<OptionCourse> options = new ArrayList<>();
+        OptionCourse option1 = new OptionCourse("012345", "incroyable 1", l);
+        OptionCourse option2 = new OptionCourse("123456z", "incroyable 2", l);
+        options.add(option1);
+        options.add(option2);
+
+        List<CompositeCourse> composites = new ArrayList<>();
+
+        Program program = new Program("un super prog", "vraimenty top", l, options, composites);
+
+        save.addBuffer(program);
+
+        save.doSave();
+    }
 }
